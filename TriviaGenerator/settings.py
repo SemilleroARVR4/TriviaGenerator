d18d6e770ae9126edcbf1ab4f3a8c5bf0542d676
Stiven Perez <fsperez@unicauca.edu.co>
2023-11-02 23:03:24 -0500

    Bootstrap Navbar

--- TriviaGenerator/settings.py
+++ TriviaGenerator/settings.py
@@ -123,7 +123,7 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # STATICFILES_DIRS=[BASE_DIR/'static']
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
